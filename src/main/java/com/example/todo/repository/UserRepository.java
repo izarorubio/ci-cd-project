08b9d9c546496ea372d3d1d5aa52d732f00fff4a
izaro.rubio <izaro.rubio@euneiz.com>
2025-01-12 04:44:36 +0100

    code changes

--- src/main/java/com/example/todo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todo.repository;
-
-import com.example.todo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
-
